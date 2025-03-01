1216b13c7a06423f34267fc722775fcf748dab3c
HamedMiri72 <hamedmiri72@gmail.com>
2025-03-01 23:36:20 +0000

    left sidebar

--- src/app/(others)/page.jsx
+++ src/app/(others)/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl text-red-600">Hello world</h1>
+      <h1 className="">Home</h1>
     </div>
   );
 }
