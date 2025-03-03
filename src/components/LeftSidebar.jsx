2c390345d63b46e3498bbc6d41d11056befbb475
HamedMiri72 <hamedmiri72@gmail.com>
2025-03-03 23:41:40 +0000

    fixing signed in and signedout button

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,33 +1,36 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 import { FaXTwitter } from "react-icons/fa6";
-import { HiHome } from "react-icons/hi"
-import {SignedIn, SignedOut, SignInButton, SignOutButton} from "@clerk/nextjs"
+import { HiHome } from "react-icons/hi";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from "@clerk/nextjs";
 
 export default function LeftSidebar() {
   return (
-    <div className='flex flex-col gap-4 p-3'>
+    <div className="flex flex-col gap-4 p-3">
+      <Link href="/">
+        <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
+      </Link>
 
-        <Link href="/">
-        <FaXTwitter className='w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200'/>
-        </Link>
+      <Link
+        href="/"
+        className="flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit"
+      >
+        <HiHome className="w-7 h-7" />
+        <span className="font-bold hidden xl:inline">Home</span>
+      </Link>
 
-        <Link href= '/' className='flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit'>
+      <SignedIn>
+        <SignOutButton className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline flex items-center justify-center" />
+      </SignedIn>
 
-        <HiHome className='w-7 h-7'/>
-        <span className='font-bold hidden xl:inline'>Home</span>
-        
-        </Link>
-
-        <button className='bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline'>
-          <SignedIn>
-            <SignOutButton/>
-          </SignedIn>
-          <SignedOut>
-            <SignInButton/>
-          </SignedOut>
-        </button>
-
+      <SignedOut>
+        <SignInButton className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline flex items-center justify-center" />
+      </SignedOut>
     </div>
-  )
+  );
 }
