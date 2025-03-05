f926853c4ee715d4382dc66e79af6b517f5a28aa
HamedMiri72 <hamedmiri72@gmail.com>
2025-03-05 14:17:42 +0000

    Add input component and update homepage

--- src/app/(others)/page.jsx
+++ src/app/(others)/page.jsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import Input from "@/components/Input";
+
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="">Home</h1>
+    <div className="min-h-screen max-w-xl mx-auto border-r border-l ">
+
+
+
+      <div className="py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+        <h2 className="text-lg sm:text-xl font-bold">Home</h2>
+      </div>
+
+      <Input/>
+
+
+      
     </div>
   );
 }
